edb5d3058359ae731b8dfb67f4454f7d12420da5
live4code <live4code@gmail.com>
2015-04-20 03:12:02 +0000

    commit exercise content by Live4Code

---
+++ app/CommentList-cm-movie.js
@@ -0,0 +1,23 @@
+var CommentList = React.createClass({
+  render: function() {
+    return (
+       
+         
+       
+    );
+  }
+});
+@@@
+prompt: Code CommentList component
+run: {command: "undo"}
+run: {command: "undo"}
+run: {command: "undo"}
+moveTo: 3:6
+type: <div className="commentList">
+moveTo: 5:6
+type: </div>
+tooltip: Create a simple div with css class commentList
+moveTo: 4:8
+type: Hello, world! I am a CommentList.
+tooltip: Some simple content for now
+prompt: Next is CommentForm
