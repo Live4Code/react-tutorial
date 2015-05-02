e645b96a241f9d25c2de1c36d9b98fa4450d8a88
live4code <live4code@gmail.com>
2015-05-02 01:12:31 +0000

    commit exercise content by Live4Code

---
+++ app/CommentForm-live.js
@@ -0,0 +1,23 @@
+var CommentForm = React.createClass({
+   
+     
+     
+     
+    
+      
+    
+    //set input DOM element's value property to empty string to clear value
+     
+  render: function() {
+    return (
+      <form className="form-inline" role="form" onSubmit={this.handleSubmit}> 
+         
+           
+         
+         
+           
+         
+      </form> 
+    );
+  }
+});
